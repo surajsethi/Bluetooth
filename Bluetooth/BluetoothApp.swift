8f98480d456b7621af91f6550a824bfba4450c3d
surajsethi <suraj.sethi1999@gmail.com>
2023-10-01 18:46:37 -0400

    Initial Commit

---
+++ Bluetooth/BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothApp.swift
+//  Bluetooth
+//
+//  Created by Suraj Sethi on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bluetooth
+//
+//  Created by Suraj Sethi on 2023-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
